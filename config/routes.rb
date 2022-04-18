0964b1af3b76a286b2f4b93cdbe4aa587cec6d40
Arnd-Kisho Langenberg <aklangenberg@gmail.com>
2022-04-18 18:18:43 +0200

    add new task

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create', as: :create_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
